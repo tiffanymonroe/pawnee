0c02eb1cb3d4ac8b1dad528b00535bcca0a39ae2
Tiffany Monroe <hello@tiffanymonroe.com>
2018-07-03 00:29:49 -0700

    added bootstrap 4

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 //components
+import Home from './Home'
 import About from './About'
 import Government from './Government'
 import Media from './Media'
 
+//React Router
+import {
+  BrowserRouter as Router,
+  Route,
+  Link } from 'react-router-dom'
+
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <About />
-        <Government />
-        <Media />
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="about">About</Link>
+          <Link to="government">Government</Link>
+          <Link to="media">Media</Link>
+        </nav>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/government" component={Government} />
+        <Route path="/media" component={Media} />
       </div>
+      </Router>
     );
   }
 }
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render(){
+    return(
+      <div className="container">
+        <h1>Welcome to Pawnee!</h1>
+        <h3>First in Friendship, Fourth in Obesity</h3>
+      </div>
+    )
+  }
+}
+
+export default Home
