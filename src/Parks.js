53e48ee7fea9240b0825794a997577248ed57a9c
Tiffany Monroe <hello@tiffanymonroe.com>
2018-07-07 12:29:32 -0700

    set props and state

--- src/Parks.js
+++ src/Parks.js
@@ -3,59 +3,59 @@ import Staff from './Staff'
 
 const season3 = require("./assets/Season_3.jpg")
 
-const staffList = [
+const staff = [
   {
     name: 'Ron Swanson',
-    department: 'Parks and Recreation',
+    department: 'Department of Parks and Recreation',
     title: 'Director',
     quote: "Never half-ass two things. Whole-ass one thing.",
     img: './assets/ron.jpg'
   },
   {
     name: 'Leslie Knope',
-    department: 'Parks and Recreation',
+    department: 'Department of Parks and Recreation',
     title: 'Deputy Director',
     quote: "What I hear when I'm being yelled at is people caring loudly at me.",
     img: './assets/leslie.jpg'
   },
   {
     name: 'Tom Haverford',
-    department: 'Parks and Recreation',
+    department: 'Department of Parks and Recreation',
     title: 'Administrator',
     quote: "Sometimes you gotta work a little so you can ball a lot.",
     img: './assets/tom.jpg'
   },
   {
     name: 'April Ludgate-Dwyer',
-    department: 'Parks and Recreation',
+    department: 'Department of Parks and Recreation',
     title: 'Assistant to Ron Swanson',
     quote: "I hate talking to people about things.",
     img: './assets/april.jpg'
   },
   {
     name: 'Donna Meagle',
-    department: 'Parks and Recreation',
+    department: 'Department of Parks and Recreation',
     title: 'Permits Secretary',
     quote: "If you don't like how I tweet, don't follow me.",
     img: './assets/donna.jpg'
   },
   {
     name: 'Jerry Gergich',
-    department: 'Parks and Recreation',
+    department: 'Department of Parks and Recreation',
     title: 'Office Manager',
     quote: "I think Comic Sans always screams fun, don't you?",
     img: './assets/jerry.jpg'
   },
   {
     name: 'Andy Dwyer',
-    department: 'Parks and Recreation',
+    department: 'Department of Parks and Recreation',
     title: 'Assistant to Leslie Knope',
     quote: "When life gives you lemons make lemonade. I read that one on a can of lemonade. I like to think it applies to life.",
     img: './assets/andy.jpg'
   },
   {
     name: 'Ann Perkins',
-    department: 'Health',
+    department: 'Department of Health',
     title: 'Public Relations Director',
     quote: "Jogging is the worst! I know it keeps you healthy, but God, at what cost?",
     img: './assets/ann.jpg'
@@ -76,6 +76,7 @@ const staffList = [
   }
 ];
 
+
 class Parks extends Component {
   render(){
     return(
@@ -86,7 +87,7 @@ class Parks extends Component {
           </div>
           <div className="col-6">
             <h4>Staff</h4>
-            <Staff staff={staffList} />
+            <Staff staff={staff} />
           </div>
 
         </div>
--- src/Staff.js
+++ src/Staff.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
 
-class Staff extends Component {
-  constructor(props) {
-    super(props)
 
-      let list = this.props.staff
-      for (let i=0; i < list.length; i++){
-        this.state = {
-          name: list[i].name,
-          department: list[i].department,
-          title: list[i].title,
-          quote: list[i].quote,
-          img: list[i].img
-        }
-      }
+class Staff extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      staff: []
+    }
+
   }
 
 
   render(){
     return(
       <div>
-        <h6>name</h6>
-        <h6>title</h6>
-        <h6>Department of </h6>
-        <h6>Quote: </h6>
-
+        <ul>
+          <li></li>
+        </ul>
       </div>
     )
   }
 }
+
+
+
 
 export default Staff
